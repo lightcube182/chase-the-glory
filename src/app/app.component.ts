75684bc66897ee66130c8fc0156923b63e203ed1
Ben Koury <bkoury@siteworx.com>
2016-10-10 00:30:33 -0400

    Adding logout button.  Remove auth logic from constructor and add to ngOnInit.  Check if player exists in current database.  If not, create player in database with info from auth object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AngularFire, FirebaseAuthState } from 'angularfire2';
+import { AngularFire, FirebaseAuthState, FirebaseListObservable } from 'angularfire2';
+
+import { PlayerService } from './player.service';
+
+import { Player } from './player';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +16,33 @@ export class AppComponent implements OnInit {
 	title = 'Chase the Glory';
 	auth: FirebaseAuthState;
 
-	ngOnInit(): void {
-
-	}
-
 	constructor(
 		private router: Router,
-		private af: AngularFire) {
-			this.af.auth.subscribe(auth => this.auth = auth);
+		private af: AngularFire,
+		private playerService: PlayerService) {
+	}
+
+	ngOnInit(): void {
+		this.af.auth.subscribe(auth => {
+			this.auth = auth;
+			let players: FirebaseListObservable<Player[]> = this.playerService.getPlayer(this.auth.uid);
+			players.subscribe(player => {
+				if (!player.length) {
+					let newPlayer: Player = new Player();
+					newPlayer.name = this.auth.auth.displayName;
+					newPlayer.uid = this.auth.uid;
+					newPlayer.image = this.auth.auth.photoURL;
+					this.playerService.create(newPlayer);
+				}
+			});
+		});
 	}
 
 	login() {
-		//this.af.auth.login();
-		console.log(this.auth);
+		this.af.auth.login();
+	}
+
+	logout() {
+		this.af.auth.logout();
 	}
 }
