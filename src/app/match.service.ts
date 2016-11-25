433fcc12172ba2dc1cb46026d9f011a662c402ac
Ben Koury <bkoury@siteworx.com>
2016-11-25 18:04:23 -0500

    New service for submitting new matches to database.

---
+++ src/app/match.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+
+import { Match } from './match';
+
+@Injectable()
+export class MatchService {
+	matches: FirebaseListObservable<Match[]>;
+
+  	constructor(
+		private af: AngularFire ) {
+			this.matches = this.af.database.list('/matches');
+	}
+
+	getMatches(): FirebaseListObservable<Match[]> {
+		return this.matches;
+	}
+
+	create(match: Match) {
+		this.matches.push(match);
+	}
+}
