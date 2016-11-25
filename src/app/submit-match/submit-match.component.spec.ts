646443c6a279543007dc0e5ea06deb2472381f6b
Ben Koury <bkoury@siteworx.com>
2016-11-25 18:03:30 -0500

    Initial functionality for submit-match component.

---
+++ src/app/submit-match/submit-match.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SubmitMatchComponent } from './submit-match.component';
+
+describe('Component: SubmitMatch', () => {
+  it('should create an instance', () => {
+    let component = new SubmitMatchComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/submit-match/submit-match.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { FirebaseListObservable } from 'angularfire2';
+
+import { PlayerService } from '../player.service';
+import { MatchService } from '../match.service';
+
+import { Player } from '../player';
+import { Match } from '../match';
+
+@Component({
+  selector: 'app-submit-match',
+  templateUrl: './submit-match.component.html',
+  styleUrls: ['./submit-match.component.css']
+})
+export class SubmitMatchComponent implements OnInit {
+	players: FirebaseListObservable<Player[]>;
+	winner: Player;
+	loser: Player;
+	match = new Match();
+
+  constructor( private playerService: PlayerService, private matchService: MatchService, private datePipe: DatePipe ) { }
+
+  ngOnInit() {
+  	this.getPlayers();
+  }
+
+  onSubmit() {
+  	let todaysDate = new Date();
+
+  	this.match.date = this.datePipe.transform(todaysDate, 'MM-dd-yyyy');
+  	this.match.leagueId = "Recreational";
+  	this.match.loserUid = this.loser.uid;
+  	this.match.loserName = this.loser.name;
+  	this.match.status = "confirmed";
+  	this.match.winnerName = this.winner.name;
+  	this.match.winnerUid = this.winner.uid;
+
+  	this.winner.wins++;
+  	this.loser.losses++;
+
+    console.log('here');
+
+  	this.playerService.update(this.winner.$key, this.winner);
+  	this.playerService.update(this.loser.$key, this.loser);
+
+  	this.matchService.create(this.match);
+  }
+
+	getPlayers(): void {
+		this.players = this.playerService.getPlayers();
+	}
+
+}
