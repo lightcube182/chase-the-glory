4b774828dbe9c161052054a9c17f2a218bff02ff
Ben Koury <bkoury@siteworx.com>
2016-11-25 18:03:03 -0500

    Stubbed out player detail component.

---
+++ src/app/player-details/player-details.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PlayerDetailsComponent } from './player-details.component';
+
+describe('Component: PlayerDetails', () => {
+  it('should create an instance', () => {
+    let component = new PlayerDetailsComponent();
+    expect(component).toBeTruthy();
+  });
+});
