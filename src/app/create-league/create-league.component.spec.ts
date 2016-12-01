3a233b1d0c83508c7488cd090f863aa43db50b07
Ben Koury <bkoury@siteworx.com>
2016-12-01 11:59:19 -0500

    New component for creating new leagues.  Leagure creator is automatically set as league admin.

---
+++ src/app/create-league/create-league.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import {TestBed, async} from '@angular/core/testing';
+import {CreateLeagueComponent} from './create-league.component';
+
+describe('Component: CreateLeague', () => {
+    it('should create an instance', () => {
+        let component = new CreateLeagueComponent();
+        expect(component).toBeTruthy();
+    });
+});
