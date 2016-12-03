ad8684b0f79a9b66d786b327a5b39857be6e5f42
Ben Koury <bkoury@siteworx.com>
2016-12-03 02:32:17 -0500

    Changed league players to Players instead of string.

--- src/app/league.ts
+++ src/app/league.ts
@@ -1,8 +1,10 @@
+import {Player} from './player';
+
 export class League {
     adminUid: string;
     $key: string;
     leagueName: string;
-    players: string[];
+    players: Player[];
     sport: string;
     type: string;
     leagueId: string;
