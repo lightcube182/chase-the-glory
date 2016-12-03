4809d9e82080e3a335716764016dec7cdf453625
Ben Koury <bkoury@siteworx.com>
2016-12-03 02:33:37 -0500

    Initial functionality of league landing.

--- src/app/league-landing/league-landing.component.ts
+++ src/app/league-landing/league-landing.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+
+import {LeagueService} from "../league.service";
+
+import {League} from "../league";
+import {Player} from "../player";
+import {FirebaseListObservable} from "angularfire2";
 
 @Component({
   selector: 'app-league-landing',
@@ -6,10 +14,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./league-landing.component.css']
 })
 export class LeagueLandingComponent implements OnInit {
+  league: League = new League();
+  leaguePlayers: FirebaseListObservable<Player[]>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private leagueService: LeagueService) { }
 
   ngOnInit() {
+    this.route.params
+        .switchMap((params: Params) => this.leagueService.getLeague(params['leagueId']))
+        .subscribe((league: League) => {
+          this.league = league;
+          this.leaguePlayers = this.leagueService.getLeaguePlayers(this.league.$key);
+        });
   }
 
 }
