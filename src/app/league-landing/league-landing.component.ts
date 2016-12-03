e346154b82d22540ba78e39e9fab841b4d6f9ad3
Ben Koury <bkoury@siteworx.com>
2016-12-03 00:41:17 -0500

    Initial push of new league-landing component.  Added to module.ts.

---
+++ src/app/league-landing/league-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-landing',
+  templateUrl: './league-landing.component.html',
+  styleUrls: ['./league-landing.component.css']
+})
+export class LeagueLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
