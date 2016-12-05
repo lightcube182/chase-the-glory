d3651bbc60927e787d5318db999d8961d6a9443e
Ben Koury <bkoury@siteworx.com>
2016-12-05 15:14:25 -0500

    update match submission status on confirm/deny match.

--- src/app/match-confirmation/match-confirmation.component.ts
+++ src/app/match-confirmation/match-confirmation.component.ts
@@ -16,24 +16,24 @@ export class MatchConfirmationComponent implements OnInit {
     auth: FirebaseAuthState;
 
     constructor(private matchService: MatchService, private playerService: PlayerService, private af: AngularFire) {
+
+    }
+
+    ngOnInit() {
         this.af.auth.subscribe(auth => {
             this.auth = auth;
             this.unconfirmedMatches = this.matchService.getMatchByConfirmation(this.auth.uid);
         });
     }
 
-    ngOnInit() {
-
-    }
-
     confirmMatch(match: Match) {
-        this.matchService.updateMatchStatus(match.$key, match.confirmationUid, "confirmed");
+        this.matchService.updateMatchStatus(match.$key, match.confirmationUid, match.submissionUid, "confirmed");
 
         this.playerService.updateWins(match.winnerUid, match.leagueId);
         this.playerService.updateLosses(match.loserUid, match.leagueId);
     }
 
     denyMatch(match: Match) {
-        this.matchService.updateMatchStatus(match.$key, match.confirmationUid, "denied");
+        this.matchService.updateMatchStatus(match.$key, match.confirmationUid, match.submissionUid, "denied");
     }
 }
