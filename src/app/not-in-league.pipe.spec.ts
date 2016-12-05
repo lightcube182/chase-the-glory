c8dd538f2ddb4df1bdac2247b3f3770903d2776b
Ben Koury <bkoury@siteworx.com>
2016-12-04 20:04:36 -0500

    New pipe to filter out players already in league.

---
+++ src/app/not-in-league.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import {TestBed, async} from '@angular/core/testing';
+import {NotInLeaguePipe} from './not-in-league.pipe';
+
+describe('NotInLeaguePipe', () => {
+    it('create an instance', () => {
+        let pipe = new NotInLeaguePipe();
+        expect(pipe).toBeTruthy();
+    });
+});
---
+++ src/app/not-in-league.pipe.ts
@@ -0,0 +1,17 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Player} from "./player";
+
+@Pipe({
+    name: 'notInLeague'
+})
+export class NotInLeaguePipe implements PipeTransform {
+
+    transform(allPlayers: Player[], leagueId: string): Player[] {
+        if (allPlayers != null) {
+            return allPlayers.filter(player => !(player.hasOwnProperty('leagues') && player.leagues.hasOwnProperty(leagueId)));
+        } else {
+            return null;
+        }
+    }
+
+}
