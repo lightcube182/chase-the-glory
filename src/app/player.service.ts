0e53ca9989d59f82488313f8e251f49ea945b5fa
Ben Koury <bkoury@siteworx.com>
2016-12-06 11:32:51 -0500

    Moved wins and losses firebase calls into observer subscription.  Added first operator to prevent infinite loops.

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,5 +1,6 @@
 import {Injectable}    from '@angular/core';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
+import 'rxjs/add/operator/first'
 
 import {Player} from './player';
 import {League} from './league';
@@ -25,17 +26,21 @@ export class PlayerService {
     updateWins(playerKey: string, leagueId: string): void {
         let winner: FirebaseObjectObservable<Player> = this.af.database.object(`/leagues/${leagueId}/players/${playerKey}`),
             currentWins: number = 0;
-        let winnerSubscription = winner.subscribe(snapshot => currentWins = snapshot.wins);
+        let winnerSubscription = winner.first().subscribe(snapshot => {
+            currentWins = snapshot.wins;
+            winner.update({wins: currentWins + 1});
+        });
         winnerSubscription.unsubscribe();
-        winner.update({wins: currentWins + 1});
     }
 
     updateLosses(playerKey: string, leagueId: string): void {
         let loser: FirebaseObjectObservable<Player> = this.af.database.object(`/leagues/${leagueId}/players/${playerKey}`),
             currentLosses: number = 0;
-        let loserSubscription = loser.subscribe(snapshot => currentLosses = snapshot.losses);
+        let loserSubscription = loser.first().subscribe(snapshot => {
+            currentLosses = snapshot.losses
+            loser.update({losses: currentLosses + 1});
+        });
         loserSubscription.unsubscribe();
-        loser.update({losses: currentLosses + 1});
     }
 
     updateLeagues(playerKey: string, league: League): void {
